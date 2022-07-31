0929da6b92dc0d262d72d80da512055fa622ef73
Gawel Sikora <gawel.sikora@gmail.com>
2022-07-31 12:48:21 +0100

    Seperate Logic to Items repository

---
+++ lib/repositories/items_repository.dart
@@ -0,0 +1,41 @@
+import 'package:cantwait28/items_models/items_models.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ItemsRepository {
+  Stream<List<ItemsModels>> getItemsStream() {
+    return FirebaseFirestore.instance
+        .collection('items')
+        .orderBy('release_date')
+        .snapshots()
+        .map((querySnapshot) {
+      return querySnapshot.docs.map((doc) {
+        return ItemsModels(
+          id: doc.id,
+          title: doc['title'],
+          imageUrl: doc['image_url'],
+          dateTime: (doc['release_date'] as Timestamp).toDate(),
+        );
+      }).toList();
+    });
+  }
+
+  Future<void> delete({required String id}) {
+    return FirebaseFirestore.instance.collection('items').doc(id).delete();
+  }
+
+  Future<void> add(
+    String title,
+    String imageURL,
+    DateTime releaseDate,
+  ) async {
+    {
+      await FirebaseFirestore.instance.collection('items').add(
+        {
+          'title': title,
+          'image_url': imageURL,
+          'release_date': releaseDate,
+        },
+      );
+    }
+  }
+}
